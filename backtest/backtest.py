bc24469604b5392fa08028ba449b7e6e822922c9
nkh1987 <nima.khodabandelou@gmail.com>
2022-07-18 02:47:26 +0430

    Refactor: remove unused files

--- backtest/backtest.py
+++
@@ -1,8 +0,0 @@
-
-
-
-
-def backtest():
-    ''' Having Defined the trading strategy and its components, we use this
-    function to perform backtest on historical data in a monte carlo sense. '''
-
