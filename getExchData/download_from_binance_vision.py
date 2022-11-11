8473a0da3deb8c70a88a81f48acfa7cf43df3cf7
nkh1987 <nima.khodabandelou@gmail.com>
2022-11-11 22:23:02 +0330

    remove cred

--- getExchData/download_from_binance_vision.py
+++ getExchData/download_from_binance_vision.py
@@ -9,7 +9,7 @@ from requests.exceptions import (HTTPError, ConnectionError, Timeout,
 import time
 
 
-year1 = ["2017", "2018", "2019", "2020", "2021"]
+year1 = ["2017", "2018", "2019", "2020", "2021", "2022"]
 year2 = ['2021']
 month1 = ["01", "02", "03", "04", "05", "06", "07",
           "08", "09", "10", "11", "12"]
