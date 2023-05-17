785b82c12b4fdae4c278bae6dccc59ebd6f3062b
Nima Khodabandelou <nima.khodabandelou@gmail.com>
2023-05-17 16:58:28 +0330

    Update HSR_List.py

--- dataManip/HSR_List.py
+++ dataManip/HSR_List.py
@@ -3,9 +3,11 @@ from unused_codes.tech_old import HSR
 def HSRList(HSR_Status1d, cl1d, ch1d, backstep, MND, asset, TS, HSR_Path,
                   HSR_file):
 
+
     if HSR_Status1d != 'D1HSRFileExist':
         HSR_Data = HSR(cl1d, ch1d, MND, backstep, asset, TS,
                                   HSR_Path, HSR_file)
+
 
         HSR_Data = [i[1] for i in HSR_Data]
     else:
