536cec3d028aa3db8a7001bfcc2863ffe554db0f
Nima Khodabandelou <nima.khodabandelou@gmail.com>
2023-05-17 16:23:06 +0330

    Update ipCheckOnDnsomatic.py

--- connection_check/ipCheckOnDnsomatic.py
+++ connection_check/ipCheckOnDnsomatic.py
@@ -7,33 +7,26 @@ from requests.exceptions import (HTTPError, ConnectionError, Timeout,
 url = "https://myip.dnsomatic.com/"
 with open('ip_report.txt', 'w') as f:
     while True:
-
         try:
             r = requests.get(url)
             r.raise_for_status()
-
         except HTTPError as err01:
             print("HTTP error: ", err01)
             # time.sleep(1)
             continue
-
         except ConnectionError as err02:
             print("Error connecting: ", err02)
             # time.sleep(1)
             continue
-
         except Timeout as err03:
             # time.sleep(1)
             print("Timeout error:", err03)
             continue
-
         except RequestException as err04:
             print("Error: ", err04)
             continue
-
         else:
             print(r.content)
-
             f.write("{0},
  {1}".format(datetime.now(),str(r.content)))
             f.write("\n")
